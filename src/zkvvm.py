808b88ae132488282b61a298de2bf5f0f32dd346
Edward Amor <edward.amor3@gmail.com>
2022-10-23 18:42:46 -0400

    fix: add _get_logger method

--- src/zkvvm.py
+++ src/zkvvm.py
@@ -24,6 +24,7 @@ class Config(collections.UserDict):
     DEFAULTS = {
         "cache_dir": pathlib.Path(user_cache_dir(__name__)),
         "log_file": pathlib.Path(user_log_dir(__name__)).joinpath(__name__ + ".log"),
+        "verbosity": logging.ERROR,
     }
 
     def __init__(self, **kwargs: Any) -> None:
@@ -32,9 +33,9 @@ class Config(collections.UserDict):
             if not k.startswith(prefix.upper()):
                 continue
             key = k.lower()[len(prefix) :]
-            env[key] = type(self.DEFAULTS[key])(v)
+            env[key] = type(self.DEFAULTS[key])(v)  # type: ignore
 
-        user = {k: type(self.DEFAULTS[k])(v) for k, v in kwargs.items()}
+        user = {k: type(self.DEFAULTS[k])(v) for k, v in kwargs.items()}  # type: ignore
         self.data = collections.ChainMap(user, env, self.DEFAULTS)  # type: ignore
 
 
@@ -59,25 +60,26 @@ class VersionManager:
         if not cache_dir.exists():
             cache_dir.mkdir(parents=True, exist_ok=True)
 
-        self.logger = logger.getChild(self.__class__.__name__)
-        self.logger.addHandler(logging.FileHandler(config["log_file"]))
+        self._logger = self._get_logger()
 
     def install(self, version: BinaryVersion, overwrite: bool = False):
         if version in self.local_versions and not overwrite:
             return
 
-        self.logger.debug(f"Installing zkVyper v{version!s} from {version.location!r}.")
+        self._logger.debug(
+            f"Installing zkVyper v{version!s} from {version.location!r}."
+        )
         resp = self._session.get(version.location, stream=True)
         fp: pathlib.Path = self._config["cache_dir"] / ("zkvyper-" + str(version))
         with fp.open("wb") as f:
             f.writelines(resp.iter_content())
-        self.logger.debug(f"Installation of v{version!s} finished.")
+        self._logger.debug(f"Installation of v{version!s} finished.")
 
     def uninstall(self, version: BinaryVersion):
         try:
             pathlib.Path(urllib.parse.urlparse(version.location).path).unlink()
         except FileNotFoundError:
-            self.logger.warning(
+            self._logger.warning(
                 f"zkVyper v{version!s} not found at {version.location!r}."
             )
 
@@ -85,7 +87,7 @@ class VersionManager:
     def remote_versions(self) -> FrozenSet[BinaryVersion]:
         """Remote zkVyper binary versions compatible with the host system."""
         remote_url = self._REMOTE_BASE_URL + self._platform_id
-        self.logger.debug(f"Fetching remote zkVyper versions from {remote_url!r}.")
+        self._logger.debug(f"Fetching remote zkVyper versions from {remote_url!r}.")
         resp = self._session.get(remote_url)
         resp.raise_for_status()
 
@@ -95,7 +97,7 @@ class VersionManager:
                 continue
             version_string = file["name"].split("-")[-1][1:]
             versions.add(BinaryVersion(version_string, location=file["download_url"]))
-        self.logger.debug(f"Found {len(versions)} zkVyper versions.")
+        self._logger.debug(f"Found {len(versions)} zkVyper versions.")
         return frozenset(versions)
 
     @property
@@ -108,6 +110,21 @@ class VersionManager:
                 continue
             versions.add(BinaryVersion(fp.name.split("-")[-1], location=fp.as_uri()))
         return frozenset(versions)
+
+    def _get_logger(self):
+        _logger = logger.getChild(self.__class__.__name__)
+        _logger.setLevel(logging.DEBUG)
+
+        if not _logger.hasHandlers():
+            fh = logging.FileHandler(self._config["log_file"])
+            fh.setFormatter(logging.Formatter("%(levelname)s:%(name)s:%(message)s"))
+            _logger.addHandler(fh)
+
+            ch = logging.StreamHandler()
+            ch.setLevel(self._config["verbosity"])
+            _logger.addHandler(ch)
+
+        return _logger
 
     @functools.cached_property
     def _platform_id(self) -> str:
