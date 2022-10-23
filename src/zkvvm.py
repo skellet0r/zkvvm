9d0f6350ddec3a4fa5f3e578ea796e4c3a3efb51
Edward Amor <edward.amor3@gmail.com>
2022-10-22 22:09:29 -0400

    fix: add VersionManager class with _platform_id property

--- src/zkvvm.py
+++ src/zkvvm.py
@@ -1,2 +1,26 @@
+import functools
+import platform
+
+
 class PlatformError(Exception):
     ...
+
+
+class VersionManager:
+    _AMD64 = ("amd64", "x86_64", "i386", "i586", "i686")
+    _ARM64 = ("aarch64_be", "aarch64", "armv8b", "armv8l")
+
+    @functools.cached_property
+    def _platform_id(self) -> str:
+        """Platform identifier.
+
+        See `Stack Overflow <https://stackoverflow.com/a/45125525>`_.
+        """
+        system, machine = platform.system(), platform.machine()
+        if system == "Linux" and machine in self._AMD64:
+            return "linux-amd64"
+        elif system == "Darwin" and machine in self._AMD64:
+            return "macosx-amd64"
+        elif system == "Darwin" and machine in self._AMD64:
+            return "macosx-arm64"
+        raise PlatformError()
