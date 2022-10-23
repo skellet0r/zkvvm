8e8485c6f795f74fd0e56df77bce1bf753b1acdb
Edward Amor <edward.amor3@gmail.com>
2022-10-22 23:38:45 -0400

    fix: add config with default cache directory

--- src/zkvvm.py
+++ src/zkvvm.py
@@ -1,8 +1,11 @@
+import collections
 import functools
+import os
 import platform
 from typing import Set
 
 import requests
+from appdirs import user_cache_dir
 from semantic_version import Version
 
 
@@ -13,10 +16,12 @@ class PlatformError(Exception):
 class VersionManager:
     _AMD64 = ("amd64", "x86_64", "i386", "i586", "i686")
     _ARM64 = ("aarch64_be", "aarch64", "armv8b", "armv8l")
+    _DEFAULT_CONFIG = {"ZKVVM_CACHE_DIR": user_cache_dir(__name__)}
     _REMOTE_BASE_URL = "https://api.github.com/repos/matter-labs/zkvyper-bin/contents/"
 
-    def __init__(self) -> None:
+    def __init__(self, **config) -> None:
         self.session = requests.Session()
+        self._config = collections.ChainMap(config, os.environ, self._DEFAULT_CONFIG)
 
     @functools.cached_property
     def remote_versions(self) -> Set[Version]:
